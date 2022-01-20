da89a06631e673c8e451966dd1cc576111655ae1
bart batalinski <bartbatalinski@gmail.com>
2022-01-19 21:22:57 -0500

    (styling): modal text trimmed and lined up properly

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle, faStar } from '@fortawesome/free-solid-svg-icons';
 
 export default function Modal(props) {
 // render all the information on the page
@@ -7,6 +7,18 @@ export default function Modal(props) {
 const {title, imgSrc, imgAlt, description, authors, linkToBook, pageCount, rating, closeModal, printType} = props;
 
 
+// function to cut down the description length a bit
+const cutString = (string, desiredLength) => {
+    let length = desiredLength;
+    if (string !== null) {
+        let trimmedStr = string.substring(0, length);
+        if(trimmedStr.length === desiredLength) {
+            trimmedStr += '...';
+        }
+    
+        return trimmedStr;
+    }
+}
 // TODO: add function to close modal and bring back book list view
 
 // TODO: use full description but clip it at a certain length and add a button to expand the description
@@ -15,27 +27,27 @@ const {title, imgSrc, imgAlt, description, authors, linkToBook, pageCount, ratin
         <article className='card'>
             <button onClick={closeModal}  className='modal-close-button'><FontAwesomeIcon className='modal-icon'  icon={ faTimesCircle} /></button>
             <div className='modal-left-section'>
-                <div className='modal-img'>
-                    <img src={imgSrc} alt={imgAlt} />
+                <div className='modal-img-container'>
+                    <img src={imgSrc} alt={imgAlt} className='modal-img' />
                 </div>
 
                 <a href={linkToBook} target="_blank" rel="noreferrer" className='modal-link-button'>Buy this {printType === 'MAGAZINE' ? 'Magazine' : 'Book'}</a>
             </div>
 
-            <div className='modal-info'>
-                <h2>{title}</h2>
-                <ul>    
+            <div className='modal-right-section'>
+                <h2 className='modal-title'>{cutString(title, 42)}</h2>
+                <ul className='modal-author-rating'>    
                     {/* in case theres no authors give me nada */}
                     {authors && authors.map((individualAuthor) => {
                     return (
                         <li><p>{individualAuthor}</p></li>
                     )
                 })}
-                    <li><p>Page Count: {pageCount}</p></li>
-                    <li><p>Rating: {rating}</p></li>
+                    {pageCount && <li><p>Page Count: {pageCount}</p></li>}
+                    {rating && <li><p><FontAwesomeIcon className='modal-icon'  icon={ faStar } /> {rating}</p></li>}
                 </ul>
 
-                <p>{description}</p>
+                <p>{cutString(description, 250)}</p>
 
             </div>
             
