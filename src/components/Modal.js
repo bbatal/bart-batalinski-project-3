7081bedd53c52d093e8bae5dcc000c202fd94cf4
bart batalinski <bartbatalinski@gmail.com>
2022-01-19 20:13:05 -0500

    (update): modal has 2 flexed containers with flexed children inside of it

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
-import './Modal.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Modal(props) {
 // render all the information on the page
@@ -11,23 +12,32 @@ const {title, imgSrc, imgAlt, description, authors, linkToBook, pageCount, ratin
 // TODO: use full description but clip it at a certain length and add a button to expand the description
 
     return (
-        <article>
-            <button onClick={closeModal}>X</button>
-            <img src={imgSrc} alt={imgAlt} />
-            <h2>{title}</h2>
-            <ul>    
-                {/* in case theres no authors give me nada */}
-                {authors && authors.map((individualAuthor) => {
-                return (
-                    <li><p>{individualAuthor}</p></li>
-                )
-            })}
-                <li><p>Page Count: {pageCount}</p></li>
-                <li><p>Rating: {rating}</p></li>
-                <li><a href={linkToBook} target="_blank" rel="noreferrer">Buy this {printType === 'MAGAZINE' ? 'Magazine' : 'Book'}</a></li>
-            </ul>
+        <article className='card'>
+            <button onClick={closeModal}  className='modal-close-button'><FontAwesomeIcon className='modal-icon'  icon={ faTimesCircle} /></button>
+            <div className='modal-left-section'>
+                <div className='modal-img'>
+                    <img src={imgSrc} alt={imgAlt} />
+                </div>
 
-            <p>{description}</p>
+                <a href={linkToBook} target="_blank" rel="noreferrer" className='modal-link-button'>Buy this {printType === 'MAGAZINE' ? 'Magazine' : 'Book'}</a>
+            </div>
+
+            <div className='modal-info'>
+                <h2>{title}</h2>
+                <ul>    
+                    {/* in case theres no authors give me nada */}
+                    {authors && authors.map((individualAuthor) => {
+                    return (
+                        <li><p>{individualAuthor}</p></li>
+                    )
+                })}
+                    <li><p>Page Count: {pageCount}</p></li>
+                    <li><p>Rating: {rating}</p></li>
+                </ul>
+
+                <p>{description}</p>
+
+            </div>
             
         </article>
     )
