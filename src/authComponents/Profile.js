d83908f9d60f179efea18e38a7d3259da44c36c0
bart batalinski <bartbatalinski@gmail.com>
2022-05-15 13:56:23 -0400

    trimmed email down in the main page

--- src/authComponents/Profile.js
+++ src/authComponents/Profile.js
@@ -1,16 +1,18 @@
 import { useAuthValue } from '../AuthContext'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebaseSetup'
+import { trimEmail } from '../utils';
 
 // styling
 import styled from 'styled-components';
 
 function Profile() {
-  const { currentUser } = useAuthValue()
-  
+  const { currentUser } = useAuthValue();
+
   return (
     <Div className='profile'>
-      <h3>Welcome {currentUser?.email == 'bartbatalinski@gmail.com' ? 'Guest' : currentUser?.email}</h3>
+      <h2>Welcome</h2>
+      <h3> {currentUser?.email === 'bartbatalinski@gmail.com' ? 'Guest' : trimEmail(currentUser?.email)}</h3>
       <Button onClick={() => signOut(auth)}>Sign Out</Button>
     </Div>
   )
@@ -18,14 +20,28 @@ function Profile() {
 
 const Div = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.4rem;
   position: absolute;
   left: 20px;
   top: 20px;
 `;
 
 const Button = styled.button`
-  border: none;
-  cursor: pointer;
-`
+  padding: 0.3em 1em;
+  border-radius: 5px;
+  background-color: transparent;
+  border: 1px solid black;
+  color: black;
+  text-transform: uppercase;
+  transition: all 0.2s ease-in;
+
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
+
 
 export default Profile
