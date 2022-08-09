76e07008b7e40ded9d820fdc1fbccd5a7048992f
bart batalinski <bartbatalinski@gmail.com>
2022-08-09 18:34:49 -0400

    added useEffect and useParams to call specific volume info

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -1,6 +1,32 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+
 
 export default function BookView() {
+
+    let { id } = useParams();
+
+    useEffect(()=>{
+          // make an API call to the Google Books API
+      axios({
+        url: `https://www.googleapis.com/books/v1/volumes/${id}`,
+        method: 'GET',
+        dataResponse: 'json',
+        params: {
+          key: 'AIzaSyB3ql9-1V1P5fP6TqBRukTZGo7Y5WQgjtk'
+        }
+  
+      })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },[])
+
   return (
-    <div>BookView</div>
+    <div>BookView {id}</div>
   )
 }
